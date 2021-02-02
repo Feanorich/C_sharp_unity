34b7c7499c0a7b4aaf250a640cb260fb18436a3a
Alexander <feanorich@yandex.ru>
2021-02-02 05:25:57 +0300

    Урок №4
    Пытался в начале, сразу, подружить написанное ранее мной с MVC
    Понял, что сложновато, решил в начале реализовать все из методички, 
    понять что к чему, и потом уже прикрутить написанные мной свои бонусы.
    
    А в методичке код с багами. ЧАсть решил, но один так и не дался :(
    
    в классе GameController, когда в Update перебираем список _interactiveObject
    происходит следующий баг
    
    Когда мы подбираем бонус с карты, он удаляется.
    И система начинает ругаться, что мы пытаемся обратиться к удаленному объекру.
    
    Пытаясь понять багу, pажал вызов метода Execute() в конструкцию try-catch. 
    При выбросе исключения он в debug пишет, что interactiveObject = null
    Но почему тогда он проходит проверку предыдущим условием? и не срабатывает continue?
    
    В общем пока ковырялся, к своим бонусам не дошел :(

---
+++ Lesson4/Scripts/InteractiveObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Geekbrains
+{
+    public abstract class InteractiveObject : MonoBehaviour, IInteractable, IDisposable, IExecute
+    {
+        public bool IsInteractable { get; } = true;
+
+        protected abstract void Interaction(Player _player);
+        /// <summary>
+        /// Выполнение объектом действия
+        /// </summary>
+        public abstract void Execute();
+
+        private void Start()
+        {
+            Action();
+        }
+
+        public void Action()
+        {
+            if (TryGetComponent(out Renderer renderer))
+            {
+                renderer.material.color = Random.ColorHSV();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!IsInteractable || !other.CompareTag("Player"))
+            {
+                return;
+            }
+            Interaction(other.GetComponent<Player>());
+
+            Dispose();
+
+        }
+
+        public void Dispose()
+        {
+            Destroy(gameObject); 
+        }
+    }
+}
