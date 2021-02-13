720decb9261443b585aafa8f31d0c68b7193c70a
Alexander <feanorich@yandex.ru>
2021-02-13 08:26:29 +0300

    Урок №6
    класс MiniMapMVC
    Реализована миникарта, через камеру (как в методичке), но переделана под MVC
    
    класс RadarMVC
    Реализован радар (как в методичке)
    Переделан под MVC
    Пофиксен баг с тем, что точки на карте двигались, из-за того что летали вверх-вниз
    На радаре отображается игрок. Реализованы разные цвета для игрока, хороших и плохих бонусов.
    
    класс InteractiveObject
    реализовано событие IsDestroyed, на которое подписано удаление объекта из списка радара и ExecuteObject

---
+++ Lesson6/Scripts/BadBonus.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using UnityEngine;
+using static UnityEngine.Random;
+
+namespace Geekbrains
+{
+    public sealed class BadBonus : InteractiveObject, IFlay, IRotation
+    {
+        public Color _color;
+        public event Action<string, Color> OnCaughtPlayerChange = delegate (string str, Color color) { };
+        private float _lengthFlay;
+        private float _speedRotation;        
+
+        private void Awake()
+        {
+            _lengthFlay = Range(1.0f, 5.0f);
+            _speedRotation = Range(10.0f, 50.0f);
+            _color = GetComponent<Renderer>().material.color;
+        }
+
+        protected override void Interaction(Player _player)
+        {
+            OnCaughtPlayerChange.Invoke(gameObject.name, _color);
+        }
+
+        public override void Execute()
+        {
+            if (!IsInteractable) { return; }
+            Flay();
+            Rotation();
+        }
+
+        public void Flay()
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x,
+                Mathf.PingPong(Time.time, _lengthFlay),
+                transform.localPosition.z);
+        }
+
+        public void Rotation()
+        {
+            transform.Rotate(Vector3.up * (Time.deltaTime * _speedRotation), Space.World);
+        }
+    }
+}
---
+++ Lesson6/Scripts/GameController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Geekbrains
+{
+    public sealed class GameController : MonoBehaviour, IDisposable
+    {
+        public PlayerType PlayerType = PlayerType.Ball;
+        private ListExecuteObject _interactiveObject;
+        private DisplayEndGame _displayEndGame;
+        private DisplayBonuses _displayBonuses;
+        private CameraController _cameraController;
+        private MiniMapMVC _miniMapController;
+        private RadarMVC _radarController;
+        private InputController _inputController;
+        private int _countBonuses;
+        private Reference _reference;
+
+        private void Awake()
+        {
+            _interactiveObject = new ListExecuteObject();
+
+            _reference = new Reference();
+
+            Player player = null;
+            if (PlayerType == PlayerType.Ball)
+            {
+                player = _reference.PlayerBall;
+            }
+            
+
+            _cameraController = new CameraController(player.transform, _reference.MainCamera.transform);
+            _interactiveObject.AddExecuteObject(_cameraController);
+
+            _miniMapController = new MiniMapMVC(player.transform, _reference.MiniMapCamera);
+            _interactiveObject.AddExecuteObject(_miniMapController);
+
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                _inputController = new InputController(player);
+                _interactiveObject.AddExecuteObject(_inputController);
+            }
+
+            _displayEndGame = new DisplayEndGame(_reference.EndGame);
+            _displayBonuses = new DisplayBonuses(_reference.Bonuse);
+            
+            _radarController = new RadarMVC(player.transform, _reference.RadarTransform);
+
+            _radarController.RegisterRadarObject(player.gameObject, _radarController.playerIco);
+
+            foreach (var o in _interactiveObject)
+            {
+                //Debug.Log($"{o.GetType()} тип О {o}");
+                if (o is BadBonus badBonus)
+                {
+                    
+
+                    badBonus.OnCaughtPlayerChange += CaughtPlayer;
+                    badBonus.OnCaughtPlayerChange += _displayEndGame.GameOver;
+
+                    badBonus.IsDestroyed += _radarController.RemoveRadarObject;
+                    badBonus.IsDestroyed += _interactiveObject.DelExecuteObject;
+
+                    _radarController.RegisterRadarObject(badBonus.gameObject, _radarController.badIco);
+                }
+
+                if (o is GoodBonus goodBonus)
+                {
+                    goodBonus.OnPointChange += AddBonuse;
+
+                    goodBonus.IsDestroyed += _radarController.RemoveRadarObject;
+                    goodBonus.IsDestroyed += _interactiveObject.DelExecuteObject;
+
+                    _radarController.RegisterRadarObject(goodBonus.gameObject, _radarController.goodIco);
+                }
+            }
+
+            _interactiveObject.AddExecuteObject(_radarController);
+
+            _reference.RestartButton.onClick.AddListener(RestartGame);
+            _reference.RestartButton.gameObject.SetActive(false);
+        }
+
+        
+
+        private void RestartGame()
+        {
+            SceneManager.LoadScene(sceneBuildIndex: 0);
+            Time.timeScale = 1.0f;
+        }
+
+        private void CaughtPlayer(string value, Color args)
+        {
+            _reference.RestartButton.gameObject.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+
+        private void AddBonuse(int value)
+        {
+            _countBonuses += 1; //value;
+            _displayBonuses.Display(_countBonuses);
+        }
+
+        private void Update()
+        {
+            for (var i = 0; i < _interactiveObject.Length; i++)
+            {
+                var interactiveObject = _interactiveObject[i];
+
+                if (interactiveObject == null)
+                {
+                    continue;
+                }
+
+                try
+                {
+                    interactiveObject.Execute();                    
+                }
+                catch (Exception exc)
+                {
+                    // когда подбираем бонус - он удаляется с карты.
+                    // эта строчка выдает, что interactiveObject = null 
+                    // Но почему тогда он проходит проверку предыдущим условием? и не срабатывает continue? 
+                    Debug.Log($"{interactiveObject} выдал багу {exc.Message}");
+                    
+                }
+                finally
+                {
+                }
+
+                
+                
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var o in _interactiveObject)
+            {
+                if (o is BadBonus badBonus)
+                {
+                    badBonus.OnCaughtPlayerChange -= CaughtPlayer;
+                    badBonus.OnCaughtPlayerChange -= _displayEndGame.GameOver;
+                }
+
+                if (o is GoodBonus goodBonus)
+                {
+                    goodBonus.OnPointChange -= AddBonuse;
+                }
+            }
+        }
+    }
+}
